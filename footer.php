b782633413e0da47b54e971ad233138cb5d23d26
Yasas Ferreira <139609496+YasasFerreira@users.noreply.github.com>
2025-03-16 06:55:34 +0000

    updated

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo '
+        <div class="row">
+            Email :- sellme@gmail.com<br>Phone :- 12345678<br>
+            @2025. All rights reserved. 
+        </div>
+    ';
+    
+?>
