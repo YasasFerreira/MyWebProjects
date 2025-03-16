116c385918990479354936ece249e4edf9ca8d4d
Yasas Ferreira <139609496+YasasFerreira@users.noreply.github.com>
2025-03-16 06:17:19 +0000

    created database connection

---
+++ connecttoDB.php
@@ -0,0 +1,12 @@
+<?php
+
+    define('HOST', 'localhost');
+    define('USERNAME', 'root');
+    define('PWD', '');
+    define('DB', 'bookdb');
+
+    $connection = mysqli_connect(HOST,USERNAME,PWD,DB);
+
+
+
+?> 
